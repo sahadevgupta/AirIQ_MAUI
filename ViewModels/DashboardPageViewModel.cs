dd05504b1b5e5344c5027d4a7ff6448f823a173b
Nilesh Gupta <ngupta@liscr.com>
2025-11-08 15:45:06 +0530

    Android dropdown position issue fixed

--- ViewModels/DashboardPageViewModel.cs
+++ ViewModels/DashboardPageViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using AirIQ.Services.Interfaces;
 using AirIQ.ViewModels.Common;
+using CommunityToolkit.Mvvm.ComponentModel;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,5 +12,14 @@ namespace AirIQ.ViewModels
 {
     public partial class DashboardPageViewModel(IViewModelParameters viewModelParameters) : BaseViewModel(viewModelParameters)
     {
+        #region [ Properties ]
+
+        [ObservableProperty]
+        private ObservableCollection<string> _sourceAirports = new ObservableCollection<string> { "Mumbai", "Bangalore", "Chennai" };
+
+        [ObservableProperty]
+        private ObservableCollection<string> _destinationAirports = new ObservableCollection<string> { "Chennai", "Mumbai", "Bangalore" };
+
+        #endregion
     }
 }
